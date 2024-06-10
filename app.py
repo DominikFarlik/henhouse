d350814ead0532b66ddb4059a48e5dd54265f219
DominikFarlik <153185929+DominikFarlik@users.noreply.github.com>
2024-06-10 14:54:04 +0200

    Ulozeni id a poctu vajec do souboru
    Pokud je stejne id cteno po urcitou dobu, je do souboru pricteno vejce k danemu id.

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
 import serial
+import json
+import os
+
+json_file = 'eggs.json'
+egg_lay_time = 50
 
 
-def write_id_to_file(id):
-    pass
+# Ulozeni sneseneho vejce do souboru
+def write_id_to_file(new_id):
+    data_list = []
+
+    # Kontrola, zda soubor existuje
+    if os.path.exists(json_file):
+        with open(json_file, 'r') as file:
+            try:
+                data_list = json.load(file)
+            except json.JSONDecodeError:
+                data_list = []
+
+    # Kontrola, zda je id v souboru
+    id_exists = False
+    for item in data_list:
+        if item['id'] == new_id:
+            item['eggs'] += 1
+            id_exists = True
+            break
+
+    # Pokud id v souboru neni, je pridano
+    if not id_exists:
+        data_list.append({'id': new_id, 'eggs': 1})
+
+    # Zapis do souboru
+    with open(json_file, 'w') as file:
+        json.dump(data_list, file, indent=4)
+
+
+# Prevod dat z ctecky na id
+def convert_data_to_id(data):
+    print(f"Raw data:{data}")
+    data = str(data)
+    data = data[8:-11]
+    converted_id = int(data, 16)
+    return converted_id
 
 
 # Parametry pro serial
@@ -21,20 +60,21 @@ counter = 0
 try:
     while True:
         if ser.in_waiting > 0:
+
+            # data z ctecky
             data = ser.read(16)
-            print(f"Raw data:{data}")
-            data = str(data)
-            data = data[8:-11]
-            data = int(data, 16)
-            if new_id == data:
-                if counter > 50:
-                    write_id_to_file(data)
+            id = convert_data_to_id(data)
+
+            if new_id == id:
+                if counter >= egg_lay_time:
+                    write_id_to_file(id)
+                    counter = 0
                 else:
                     counter += 1
             else:
-                new_id = data
+                new_id = id
                 counter = 1
-            print(f"Id: {data} {counter}")
+            print(f"Id: {id} {counter}")
 
 except KeyboardInterrupt:
     print("Port uzavren")
