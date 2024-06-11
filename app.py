771634c83ea67940309cf7270a9c9f78af16d0b6
Dominik Farlik <xfarlik@mendelu.cz>
2024-06-11 13:05:01 +0200

    Prvni navrh reseni kolize cipu

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ import os
 json_file = 'eggs.json'
 egg_lay_time = 50
 
+# Parametry pro serial
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=9600,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=5
+)
+
 
 # Ulozeni sneseneho vejce do souboru
 def write_id_to_file(new_id):
@@ -44,41 +54,46 @@ def convert_data_to_id(data):
     return converted_id
 
 
-# Parametry pro serial
-ser = serial.Serial(
-    port='COM3',
-    baudrate=9600,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    timeout=5
-)
+if __name__ == "__main__":
+    last_id = None
+    current_id = None
+    counter = 0
+    colliding_id = None
+    colliding_counter = 0
 
-new_id = 0
-counter = 0
+    try:
+        while True:
+            if ser.in_waiting > 0:
+                data = ser.read(16)
+                new_id = convert_data_to_id(data)
 
-try:
-    while True:
-        if ser.in_waiting > 0:
-
-            # data z ctecky
-            data = ser.read(16)
-            id = convert_data_to_id(data)
-
-            if new_id == id:
-                if counter >= egg_lay_time:
-                    write_id_to_file(id)
-                    print(f"Slepice {id} prave snesla vejce.")
-                    counter = 0
+                if new_id == current_id:
+                    if counter >= egg_lay_time:
+                        write_id_to_file(current_id)
+                        print(f"Slepice {current_id} prave snesla vejce.")
+                        counter = 0
+                    else:
+                        counter += 1
+                elif new_id == colliding_id:
+                    if colliding_counter >= egg_lay_time:
+                        write_id_to_file(colliding_id)
+                        print(f"Slepice {colliding_id} prave snesla vejce.")
+                        colliding_counter = 0
+                    else:
+                        colliding_counter += 1
                 else:
-                    counter += 1
-            else:
-                new_id = id
-                counter = 1
-            print(f"Id: {id} {counter}")
+                    # New ID encountered, swap current and colliding states
+                    colliding_id = current_id
+                    colliding_counter = counter
+                    current_id = new_id
+                    counter = 1
 
-except KeyboardInterrupt:
-    print("Port uzavren")
+                print(f"Current ID: {current_id}, Counter: {counter}")
+                if colliding_id:
+                    print(f"Colliding ID: {colliding_id}, Counter: {colliding_counter}")
 
-finally:
-    ser.close()
+    except KeyboardInterrupt:
+        print("Port uzavren")
+
+    finally:
+        ser.close()
