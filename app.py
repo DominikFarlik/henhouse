fbd63a2c937dc35bfa15f8eeea1694e67ba4426f
Dominik Farlik <xfarlik@mendelu.cz>
2024-06-12 19:11:11 +0200

    Ruff added

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-import serial  # type: ignore
 import json
-import os
 import logging
+import os
 
-json_file = 'eggs.json'
+import serial  # type: ignore
+
+json_file = "eggs.json"
 egg_lay_time = 50
 
 # Params for port
 ser = serial.Serial(
-    port='/dev/ttyUSB0',
+    port="/dev/ttyUSB0",
     baudrate=9600,
     bytesize=serial.EIGHTBITS,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
-    timeout=5
+    timeout=5,
 )
 
 
@@ -23,7 +24,7 @@ def write_id_to_file(id_to_save: int) -> None:
 
     # Checks if file exists
     if os.path.exists(json_file):
-        with open(json_file, 'r') as file:
+        with open(json_file, "r") as file:
             try:
                 data_list = json.load(file)
             except json.JSONDecodeError:
@@ -32,23 +33,23 @@ def write_id_to_file(id_to_save: int) -> None:
     # Check if id is already in file
     id_exists = False
     for item in data_list:
-        if item['id'] == id_to_save:
-            item['eggs'] += 1
+        if item["id"] == id_to_save:
+            item["eggs"] += 1
             id_exists = True
             break
 
     # If id isn't already in file add
     if not id_exists:
-        data_list.append({'id': id_to_save, 'eggs': 1})
+        data_list.append({"id": id_to_save, "eggs": 1})
 
     # Writing to a file
-    with open(json_file, 'w') as file:
+    with open(json_file, "w") as file:
         json.dump(data_list, file, indent=4)
 
 
 # Converting raw input to id
 def convert_data_to_id(data_to_convert: bytes) -> int:
-    converted_data = data_to_convert.decode('ascii')
+    converted_data = data_to_convert.decode("ascii")
     raw_id = converted_data[3:11]
     converted_id = int(raw_id, 16)
     return converted_id
@@ -56,9 +57,11 @@ def convert_data_to_id(data_to_convert: bytes) -> int:
 
 if __name__ == "__main__":
     # Logging config
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s - %(levelname)s - %(message)s',
-                        handlers=[logging.StreamHandler(), logging.FileHandler('egg_lay_log.log')])
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[logging.StreamHandler(), logging.FileHandler("egg_lay_log.log")],
+    )
 
     last_id = 0
     current_id = 0
