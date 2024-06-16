d7f5f1889878d5afe3671a167263e048b9f3cdd6
Dominik Farlik <xfarlik@mendelu.cz>
2024-06-16 21:19:50 +0200

    Added thread lock to prevent collisions with db

--- app.py
+++ app.py
@@ -101,22 +101,22 @@ class EventProcessor:
         self.running = False
 
 
-def write_event_to_db(
-    chip_id: int, reader_id: str, event_time: str, event_type: str
-) -> None:
-    """Writes received data to the database with the current timestamp."""
-    try:
-        with sqlite3.connect("henhouse.db") as connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                "INSERT INTO events (chip_id, reader_id, event_type, event_time) VALUES (?, ?, ?, ?)",
-                (chip_id, reader_id, event_type, event_time),
-            )
-            connection.commit()
-    except sqlite3.Error as e:
-        logging.error(f"Database error: {e}")
-    except Exception as e:
-        logging.error(f"Unexpected error: {e}")
+def write_event_to_db(chip_id: int, reader_id: str, event_time: str, event_type: str) -> None:
+    """Writes received data to the database."""
+    lock = threading.Lock()
+    with lock:
+        try:
+            with sqlite3.connect("henhouse.db") as connection:
+                cursor = connection.cursor()
+                cursor.execute(
+                    "INSERT INTO events (chip_id, reader_id, event_type, event_time) VALUES (?, ?, ?, ?)",
+                    (chip_id, reader_id, event_type, event_time),
+                )
+                connection.commit()
+        except sqlite3.Error as e:
+            logging.error(f"Database error: {e}")
+        except Exception as e:
+            logging.error(f"Unexpected error: {e}")
 
 
 def convert_data_to_id(data_to_convert: bytes) -> int:
